21ad6bb0fff29474369b64cbe5098924793eef42
xBA5ED <0xBA5ED@protonmail.com>
2022-06-07 00:49:37 +0200

    Build some Mohandar and GigantusChad packs

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '99ced887'
+version = '0.1.0+dev.99ced887'
+linkified_version = '0.1.0+dev.[99ced887](https://github.com/maunium/stickerpicker/commit/99ced8878aaf89a47dd8bdb592025ae2160d6f07)'
